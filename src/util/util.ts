81691db700d5399dba05e7dcbcf83b139aefe69e
JeremAIh <esiebomaj@gmail.com>
2021-10-24 14:59:39 +0100

    added udacity project solution

---
+++ src/util/util.ts
@@ -0,0 +1,52 @@
+import fs from "fs";
+import Jimp = require("jimp");
+
+// filterImageFromURL
+// helper function to download, filter, and save the filtered image locally
+// returns the absolute path to the local image
+// INPUTS
+//    inputURL: string - a publicly accessible url to an image file
+// RETURNS
+//    an absolute path to a filtered image locally saved file
+
+export async function filterImageFromURL(inputURL: string): Promise<string> {
+  const outpath = "/tmp/filtered." + Math.floor(Math.random() * 2000) + ".jpg";
+
+  const hello = await Jimp.read(inputURL)
+    .then((photo) => {
+      return photo
+        .resize(256, 256) // resize
+        .quality(60) // set JPEG quality
+        .greyscale() // set greyscale
+        .write(__dirname + outpath);
+    })
+    .catch((err) => console.log(err));
+  return __dirname + outpath;
+}
+
+// export async function filterImageFromURL(inputURL: string): Promise<string> {
+//   const outpath = "/tmp/filtered." + Math.floor(Math.random() * 2000) + ".jpg";
+
+//   Jimp.read(inputURL, async (error, photo) => {
+//     if (error) {
+//       console.log(error, "hi");
+//     }
+//     photo
+//       .resize(256, 256) // resize
+//       .quality(60) // set JPEG quality
+//       .greyscale() // set greyscale
+//       .write(__dirname + outpath);
+//   });
+//   return __dirname + outpath;
+// }
+
+// deleteLocalFiles
+// helper function to delete files on the local disk
+// useful to cleanup after tasks
+// INPUTS
+//    files: Array<string> an array of absolute paths to files
+export async function deleteLocalFiles(files: Array<string>) {
+  for (let file of files) {
+    fs.unlinkSync(file);
+  }
+}
